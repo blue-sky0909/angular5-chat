9df080d6b67a39f22b2c65bcf529664262ed1c41
blue-sky0909 <daiki0115@yandex.com>
2018-04-13 19:09:49 +0200

    minor fix

--- server/models/user.ts
+++ server/models/user.ts
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     email: { type: 'String', required: true },
     username: { type: 'String', required: true },
     password: { type: 'String', required: true },
-    role: { type: 'String', default: "admin" },
+    role: { type: 'String', default: "guest" },
 },
 {
     timestamps: {
